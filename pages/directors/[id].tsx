a56bca8e1128dd197f662838b1dfebdfb2d906f3
Ma1y0 <matyas.barr@gmail.com>
2022-12-08 10:37:10 +0100

    Dorectors Page Alfa

--- pages/directors/[id].tsx
+++ pages/directors/[id].tsx
@@ -1,15 +1,31 @@
-import { collectionGroup } from "firebase/firestore"
+import { collectionGroup, getDocs, getFirestore, limit, query, where } from "firebase/firestore"
 import { useRouter } from "next/router"
+import { useState } from "react"
+import { DirectorsToJSON } from "../../lib/firebase"
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
-    const ref = collectionGroup()
+    const { id } = context.query
+    const ref = collectionGroup(getFirestore(), "directors")
+    const directorQuery = query(
+        ref,
+        where("name", "==", id),
+        limit(1)
+    )
+
+    const director = (await getDocs(directorQuery)).docs.map(DirectorsToJSON)
+
+    return {
+        props: { director }
+    }
 }
 
-export default function Director() {
-    const router = useRouter()
-    const { id } = router.query
+export default function Director(props: any) {
+    const [director, setDirector] = useState(props.director[0])
+
 
     return (
-        <main>{id}</main>
+        <main>
+            <h1>{director.name}</h1>
+        </main>
     )
 }
