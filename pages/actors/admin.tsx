d53c3ef4b433130bcd0b9ed4621f3dfd1e0d3b3a
Ma1y0 <matyas.barr@gmail.com>
2022-12-08 11:02:04 +0100

    actors page alfa

---
+++ pages/actors/admin.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { doc, setDoc, getFirestore, Timestamp } from 'firebase/firestore';
+
+export default function Admin() {
+    const [actorName, setActorName] = useState("")
+
+    const onChange = (e: Event) => {
+        const target = e.target as HTMLInputElement
+        const val = target.value
+        setActorName(val)
+    }
+
+    const onSubmit = async (e: Event) => {
+        e.preventDefault()
+
+        try {
+            await setDoc(doc(getFirestore(), "actors", actorName), {
+                name: actorName,
+                createdAt: Timestamp.fromDate(new Date())
+            })
+
+            setActorName("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <main>
+            <form onSubmit={onSubmit}>
+            <div className="mb-6">
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Director's Name</label>
+                <input value={actorName} onChange={onChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+            </div>
+            {/* <div className="mb-6">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 required" />
+            </div> */}
+            <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+            </form>
+        </main>
+    )
+}
--- pages/actors/index.tsx
+++ pages/actors/index.tsx
@@ -1,7 +1,32 @@
-export default function actors() {
+import { collectionGroup, getDocs, getFirestore, limit, orderBy, query } from "firebase/firestore"
+import { DirectorsToJSON } from "../../lib/firebase"
+import { useState } from "react"
+import DirectorsList from "../../components/DirectorsList"
+import Link from "next/link"
+
+const limitC = 200
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+    const ref = collectionGroup(getFirestore(), "actors")
+    const actorsQuery = query(
+        ref,
+        orderBy('createdAt', 'desc'),
+        limit(limitC)
+    )
+
+    const actors = (await getDocs(actorsQuery)).docs.map(DirectorsToJSON)
+
+    return {
+        props: { actors }
+    }
+}  
+
+export default function Actors(props: any) {
+    const [directors, setDirectors] = useState(props.actors)
+
     return (
-        <main>
-            Actors
+        <main className="flex justify-center">
+            <DirectorsList directors={directors} admin={false} />
         </main>
     )
 }
